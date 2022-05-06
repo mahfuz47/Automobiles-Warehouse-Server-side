b16c3e0547d1340837d944d1344be96af5c66ec6
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-06 15:42:25 +0600

    single id reading operation added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,13 @@ async function run() {
       const cursor = carsCollection.find(query);
       const users = await cursor.toArray();
       res.send(users);
+    });
+
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const cars = await carsCollection.findOne(query);
+      res.send(cars);
     });
   } finally {
   }
