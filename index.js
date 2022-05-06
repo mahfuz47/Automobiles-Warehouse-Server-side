f39a46b238fe5b862acf8d8719005d7209a425e9
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-07 01:31:09 +0600

    delete function issues solved

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function run() {
     //DELETE DATA
     //------------
 
-    app.delete("/service/:id", async (req, res) => {
+    app.delete("/cars/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await carsCollection.deleteOne(query);
